69b564b77f00be5da3d79ed3b2697f3bb6e9bfd6
Wonwoo Choi <onechoo1106@gmail.com>
2022-09-15 14:25:38 +1000

    first commit

--- climate.py
+++ climate.py
@@ -1,8 +1,25 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+
 years = []
 co2 = []
 temp = []
+
+connection=sqlite3.connect("week8test\WS8\climate.db")#from local 
+cursor=connection.cursor()
+cursor.execute("SELECT * FROM ClimateData")
+result=cursor.fetchall()
+for row in result:
+    #print(row[0])#year
+    years.append( row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+print(years)
+print(co2)
+print (temp)
+
 
 
 plt.subplot(2, 1, 1)
